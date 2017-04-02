6b91a6e72c95430b9458f80fe03d17108abe5639
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-04-02 23:01:18 +0530

    Add the new scopes for github

--- profiles/adapters.py
+++ profiles/adapters.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
-class SocialAccountAdapter(DefaultSocialAccountAdapter):
+class NotificationsSocialAccountAdapter(DefaultSocialAccountAdapter):
     """
     Comment goes here
     """
-    def get_login_redirect_url(self, request):
+    def get_connect_redirect_url(self, request, socialaccount):
         if request.user.is_authenticated():
             return reverse('profiles_dashboard', args=[request.user.username])
--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from app.core.models import Base
 
@@ -7,5 +10,17 @@ class UserProfile(Base):
     """
     Comment goes here.
     """
-
+    user = models.OneToOneField(User, related_name='profile',
+                                on_delete=models.CASCADE)
     last_logged_in = models.DateTimeField(db_index=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
